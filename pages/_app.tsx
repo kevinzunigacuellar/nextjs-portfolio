b8685c2861f4e16bafc05b8416e05a0f9ca1fa38
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-01-22 18:03:35 -0500

    install husky,prettier and eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.css";
+import type { AppProps } from "next/app";
 
+/** This is the app component. It is the root of the application.
+ * @return {JSX.Element}: The JSX element to render.
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
