6a067ef1ff829428d7d252c5fba9551d95297e33
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-01-23 14:19:11 -0500

    add semantic html to Book component

--- components/Book.tsx
+++ components/Book.tsx
@@ -9,33 +9,30 @@ interface BookProps {
 
 export default function Book({ img, title, author, year, comment }: BookProps) {
   return (
-    <article className="font-body max-w-md border sm:w-full sm:max-w-4xl bg-white rounded-xl overflow-hidden dark:bg-gray-800/90 dark:border-gray-700 transition-colors">
-      <div className="sm:flex">
-        <div className="max-w-md p-4 pb-0 sm:pb-4 sm:pr-1">
-          <div className="relative aspect-[3/4] sm:w-48">
-            <Image
-              className="absolute w-full h-full rounded-xl"
-              src={img}
-              alt={title}
-              layout="fill"
-              objectFit="fill"
-              placeholder="blur"
-            />
-          </div>
-        </div>
-        <div className="py-5 px-6 w-full">
-          <h2 className="font-semibold pb-1.5 text-xl dark:text-white text-gray-900">
+    <li className="font-body max-w-md border sm:w-full sm:max-w-4xl bg-white rounded-xl overflow-hidden dark:bg-gray-800/90 dark:border-gray-700 transition-colors">
+      <article className="sm:flex px-6 pt-6 sm:pb-6">
+        <figure className="block shrink-0 sm:w-40 max-w-md rounded-xl overflow-hidden shadow-lg">
+          <Image
+            className="w-full h-full aspect-[3/4]"
+            src={img}
+            alt={title}
+            layout="responsive"
+            placeholder="blur"
+          />
+        </figure>
+        <div className="py-6 sm:py-0 sm:px-6 w-full">
+          <h2 className="font-semibold mb-1.5 text-xl dark:text-white text-gray-900">
             {title}
           </h2>
           <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-            <span className="text-indigo-600/90 dark:text-cyan-500 font-medium">
+            <span className="text-indigo-600/95 dark:text-cyan-500 font-medium">
               {author}
             </span>{" "}
             <span>&middot;</span> <span>{year}</span>
           </p>
-          <p className="text-gray-500 py-2 dark:text-gray-400 leading-relaxed">{`"${comment}"`}</p>
+          <p className="py-2 text-gray-500 dark:text-gray-400 leading-relaxed">{`"${comment}"`}</p>
         </div>
-      </div>
-    </article>
+      </article>
+    </li>
   );
 }
