67697fde4da1499395f7b2c7cc11c638f50841e2
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-01-23 00:48:39 -0500

    deactivated js-docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "require-jsdoc": "off",
+  },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 
-/** This is the app component. It is the root of the application.
- * @return {JSX.Element}: The JSX element to render.
- */
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
