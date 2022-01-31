a245a88dfb6f9965e3c984b639e9ea930b89b2fd
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-01-31 08:54:16 -0500

    add books to bookshelf page

--- pages/bookshelf.tsx
+++ pages/bookshelf.tsx
@@ -5,8 +5,8 @@ import type { NextPage } from "next";
 
 const Bookshelf: NextPage = () => {
   return (
-    <div className="dark">
-      <div className="grid grid-cols-1 justify-items-center md:justify-items-start gap-10 px-10 py-10 bg-gray-50 dark:bg-gray-900">
+    <div>
+      <ul className="grid grid-cols-1 justify-items-center md:justify-items-start gap-10 px-10 py-10 dark:bg-slate-900">
         <Book
           title="Eloquent JavaScript"
           author="Marijn Haverbeke"
@@ -16,12 +16,40 @@ const Bookshelf: NextPage = () => {
         />
         <Book
           title="Aprendiendo Git"
-          author="Miguel Angel Duran"
+          author="Miguel Ángel Duran"
           comment="I bought this book on presale to support one of my favorites twitch streamers. I highly recommend it regardless of your knowledge of git."
           img={AprendiendoGit}
           year={2021}
         />
-      </div>
+        <Book
+          title="Eloquent JavaScript"
+          author="Marijn Haverbeke"
+          comment="One of the best JavaScript books I have ever read. Even tough I finish this book a while ago. I still keep a hard copy in bookshelf to keep me always in day one mode."
+          img={eloquentJavaScript}
+          year={2018}
+        />
+        <Book
+          title="Aprendiendo Git"
+          author="Miguel Ángel Duran"
+          comment="I bought this book on presale to support one of my favorites twitch streamers. I highly recommend it regardless of your knowledge of git."
+          img={AprendiendoGit}
+          year={2021}
+        />
+        <Book
+          title="Eloquent JavaScript"
+          author="Marijn Haverbeke"
+          comment="One of the best JavaScript books I have ever read. Even tough I finish this book a while ago. I still keep a hard copy in bookshelf to keep me always in day one mode."
+          img={eloquentJavaScript}
+          year={2018}
+        />
+        <Book
+          title="Aprendiendo Git"
+          author="Miguel Ángel Duran"
+          comment="I bought this book on presale to support one of my favorites twitch streamers. I highly recommend it regardless of your knowledge of git."
+          img={AprendiendoGit}
+          year={2021}
+        />
+      </ul>
     </div>
   );
 };
