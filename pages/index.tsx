6a48a1c26e4f7fc95cfd0c2bd950231662e67453
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-02-01 22:29:31 -0500

    fix semantic html in hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,27 +6,24 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <Container>
-      <section className="pb-10 sm:flex sm:items-center sm:justify-between">
-        <div className="max-w-sm sm:m-0 order-2">
-          <div className="block w-40 h-40 md:w-44 md:h-44 rounded-full overflow-hidden">
-            <Image
-              className="w-full h-full aspect-square"
-              src={profilePic}
-              alt="Picture of the author"
-              placeholder="blur"
-              layout="responsive"
-            />
-          </div>
+      <section className="mb-10 sm:flex sm:items-center sm:justify-between">
+        <div className="block sm:m-0 order-2 w-44 h-auto rounded-full overflow-hidden">
+          <Image
+            className="w-full h-full aspect-square"
+            src={profilePic}
+            alt="Picture of the author"
+            placeholder="blur"
+            layout="responsive"
+          />
         </div>
-        <header>
-          <div className="my-6 sm:my-4">
-            <h1 className="font-bold mb-2 text-gray-900 tracking-tight text-3xl sm:text-4xl dark:text-white">
-              Kevin Zuniga Cuellar
-            </h1>
-            <h2 className="py-1.5 max-w-lg font-medium text-gray-700 dark:text-gray-300">
-              Graduate Research Assistant at University of Vermont
-            </h2>
-          </div>
+        <header className="mt-6 sm:my-0">
+          <h1 className="font-bold mb-2 text-gray-900 tracking-tight text-3xl sm:text-4xl dark:text-white">
+            Kevin Zuniga Cuellar
+          </h1>
+          <h2 className="mb-4 font-medium text-gray-700 dark:text-gray-300">
+            Graduate Research Assistant at University of Vermont
+          </h2>
+
           <p className="w-full font-normal leading-7 text-gray-600 sm:max-w-sm md:max-w-xl dark:text-gray-400">
             Welcome to my slice of the internet. I am a graduate student in
             mechanical engineering at University of Vermont. In my free time I
