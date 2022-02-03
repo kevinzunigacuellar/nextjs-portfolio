60b116d559a7405d248e3c0b71ddac5a147f0739
Kevin Z - Razer <kzunigac@uvm.edu>
2022-02-02 20:27:07 -0500

    set up next-auth client side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,34 @@
 import "styles/globals.css";
 import { motion } from "framer-motion";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 import Navbar from "components/Navbar";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+  router,
+}: AppProps) {
   return (
     <ThemeProvider
       attribute="class"
       disableTransitionOnChange
       defaultTheme="system"
     >
-      <Navbar />
-      <main className="min-h-screen w-screen bg-white dark:bg-gray-900/95">
-        <motion.div
-          className="mx-auto max-w-4xl px-6 py-20 pb-36"
-          key={router.route}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </main>
+      <SessionProvider session={session}>
+        <Navbar />
+        <main className="min-h-screen w-screen bg-white dark:bg-gray-900/95">
+          <motion.div
+            className="mx-auto max-w-4xl px-6 py-20 pb-36"
+            key={router.route}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </main>
+      </SessionProvider>
     </ThemeProvider>
   );
 }
