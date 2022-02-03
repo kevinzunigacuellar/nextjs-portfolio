1c4555f1a21d7ced2960000ea012ba82907501eb
Kevin Z - Razer <kzunigac@uvm.edu>
2022-02-02 20:27:51 -0500

    create loading spinner and login button

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,22 @@
+const LoadingSpinner = () => {
+  return (
+    <svg
+      className="ml-2 block h-4 w-4 animate-spin text-blue-900"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle
+        className="stroke-current stroke-[4px] opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+      ></circle>
+      <path
+        className="fill-current"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  );
+};
+
+export default LoadingSpinner;
---
+++ components/LogInOutButton.tsx
@@ -0,0 +1,31 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import LoadingSpinner from "components/LoadingSpinner";
+import Github from "components/icons/Github";
+const LogInOutButton = () => {
+  const { status } = useSession();
+
+  const handleClick = async () => {
+    if (status === "authenticated") await signOut({ redirect: false });
+    if (status === "unauthenticated") await signIn("github");
+  };
+
+  if (status === "loading") return <LoadingSpinner />;
+
+  return (
+    <button
+      className="dark:bg-bg-700 focus:shadow-outline text-whitee flex shrink-0 items-center rounded-full bg-gray-900 px-4 py-1 font-medium text-white"
+      onClick={handleClick}
+    >
+      {status === "authenticated" ? (
+        "Sign out"
+      ) : (
+        <>
+          <Github className="mr-2 inline-block h-auto w-4 fill-white" /> Sign in
+          with GitHub
+        </>
+      )}
+    </button>
+  );
+};
+
+export default LogInOutButton;
