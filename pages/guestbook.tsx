e4b9c69f2b37b913e7f9d4735ea26a830958df4f
Kevin Z - Razer <kzunigac@uvm.edu>
2022-02-06 01:34:37 -0500

    refactor guestbook and add disabled styles

--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -1,8 +1,9 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useSession, signIn } from "next-auth/react";
+import type { NextPage } from "next";
 import Container from "components/Container";
 import LoadingSpinner from "components/LoadingSpinner";
-import { useSession, signIn } from "next-auth/react";
 import Github from "components/icons/Github";
-import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
   message: string;
@@ -12,13 +13,10 @@ function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-import type { NextPage } from "next";
 const Guestbook: NextPage = () => {
-  const { status } = useSession();
-
   return (
     <Container pageTitle="Guestbook" title="Guestbook - Kevin Zuniga Cuellar">
-      <section className="mb-10 rounded-xl border border-blue-200 bg-blue-100 p-6 dark:border-blue-600 dark:bg-blue-900 ">
+      <section className="mb-10 rounded-xl border border-blue-200 bg-blue-100 p-6 dark:border-blue-500 dark:bg-blue-800/90">
         <h2 className="mb-2 text-xl font-semibold text-gray-900 dark:text-white">
           Sign the Guestbook
         </h2>
@@ -26,9 +24,7 @@ const Guestbook: NextPage = () => {
           Feel free to share a message with a future visitor or tell me what you
           like the most about my website.
         </p>
-        {status === "loading" && <LoadingSpinner />}
-        {status === "authenticated" && <GuestbookForm />}
-        {status === "unauthenticated" && <LogInWithGithub />}
+        <GuestbookBody />
       </section>
     </Container>
   );
@@ -57,17 +53,17 @@ const GuestbookForm = () => {
         })}
         placeholder="Your message..."
         disabled={isSubmitting}
-        className="block w-full rounded-lg border border-slate-300 bg-white py-2 pl-3 pr-28 placeholder-slate-400 shadow-sm invalid:border-pink-500 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-blue-400 dark:focus:ring-blue-600 sm:pr-32"
+        className="block w-full rounded-lg border border-gray-300 bg-white py-2 pl-3 pr-28 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-400 dark:focus:ring-blue-600 sm:pr-32"
       />
       <button
         type="submit"
         disabled={isSubmitting}
-        className="bg-white-500 absolute top-[3px] right-[3px] flex w-24 items-center justify-center rounded-md bg-gray-200 px-4 py-2 font-semibold leading-5 text-gray-900 transition-colors hover:bg-gray-300 focus:outline-none  focus:ring-2 focus:ring-blue-400 focus:ring-offset-1 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800 sm:w-28"
+        className="bg-white-500 absolute top-[3px] right-[3px] flex w-24 items-center justify-center rounded-md bg-gray-200 px-4 py-2 font-semibold leading-5 text-gray-900 transition-colors hover:bg-gray-300 focus:outline-none focus:ring-2  focus:ring-blue-400 focus:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-400 dark:focus:ring-offset-gray-800 dark:disabled:bg-gray-900 dark:disabled:text-gray-400 sm:w-28"
       >
         Sign
         {isSubmitting && <LoadingSpinner />}
       </button>
-      <span className="text-sm font-medium text-indigo-600 dark:text-white">
+      <span className="text-xs font-medium text-indigo-600 dark:text-indigo-300">
         {errors.message?.message}
       </span>
     </form>
@@ -84,4 +80,19 @@ const LogInWithGithub = () => {
       with GitHub
     </button>
   );
+};
+
+const GuestbookBody = () => {
+  const { status } = useSession();
+
+  if (status === "loading")
+    return (
+      <div className="flex w-full justify-center">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (status === "unauthenticated") return <LogInWithGithub />;
+
+  return <GuestbookForm />;
 };
