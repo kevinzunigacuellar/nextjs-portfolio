6fcbe7aaa3b443582a6300321919b49894eb1983
Kevin Z - Razer <kzunigac@uvm.edu>
2022-02-06 01:35:16 -0500

    add selection styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({
     >
       <SessionProvider session={session}>
         <Navbar />
-        <main className="min-h-screen w-screen bg-white dark:bg-gray-900/95">
+        <main className="min-h-screen w-screen bg-white antialiased selection:bg-teal-300 selection:text-teal-900 dark:bg-gray-900/95 dark:selection:bg-sky-300 dark:selection:text-sky-900">
           <motion.div
             className="mx-auto max-w-4xl px-6 py-20 pb-36"
             key={router.route}
