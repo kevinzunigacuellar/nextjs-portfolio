e557d47e903e154af6883a29429a1b55f3b631fe
Kevin Z - Razer <kzunigac@uvm.edu>
2022-02-08 18:32:35 -0500

    create guestbook api

---
+++ pages/api/guestbook/[id].ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import prisma from "lib/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const entry = await prisma.guestbook.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!entry) return res.status(404).send(`No entry found for id ${id}`);
+
+  if (req.method === "GET") {
+    return res.json({
+      id: entry.id.toString(),
+      body: entry.body,
+      created_by: entry.created_by,
+      updated_at: entry.updated_at,
+    });
+  }
+
+  const session = await getSession({ req });
+  if (!session) return res.status(403).send("Requires authentication");
+
+  const { email } = session.user as { email: string };
+
+  if (email !== entry.email) {
+    return res.status(403).send("Unauthorized");
+  }
+
+  if (req.method === "DELETE") {
+    await prisma.guestbook.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    return res.status(204).json({});
+  }
+
+  if (req.method === "PUT") {
+    const body = (req.body.body || "").slice(0, 500);
+
+    await prisma.guestbook.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        body,
+        updated_at: new Date().toISOString(),
+      },
+    });
+
+    return res.status(201).json({
+      ...entry,
+      body,
+    });
+  }
+
+  return res.send("Method not allowed :(");
+}
