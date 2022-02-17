77e67b3f29b4204eb046c52245bb4a88fd655b26
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-02-17 18:44:01 -0500

    modify darkmode selection colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     >
       <SessionProvider session={session}>
         <Navbar />
-        <main className="min-h-screen w-screen bg-white antialiased selection:bg-teal-300 selection:text-teal-900 dark:bg-gray-900/95 dark:selection:bg-sky-300 dark:selection:text-sky-900">
+        <main className="min-h-screen w-screen bg-white antialiased selection:bg-teal-300 selection:text-teal-900 dark:bg-gray-900/95 dark:selection:bg-indigo-300 dark:selection:text-indigo-900">
           <div className="mx-auto max-w-4xl px-8 pb-36 pt-20">
             <Component {...pageProps} />
             <div className="mt-10 border-t pt-6 dark:border-gray-700">
