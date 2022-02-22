e04212d5f809c078708b80a02a6ec4969024bc9d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-02-22 01:20:53 -0500

    add darkmode shadows

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -4,7 +4,7 @@ interface TooltipProps {
 
 const Tooltip = ({ text }: TooltipProps) => {
   return (
-    <span className="absolute bottom-20 w-auto min-w-max translate-y-10 scale-0 rounded-lg border border-gray-300 bg-white px-2 py-1 text-xs font-medium text-gray-500 opacity-0 shadow transition-all duration-200 group-hover:translate-y-2 group-hover:scale-100 group-hover:opacity-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 group-hover:sm:translate-y-1">
+    <span className="absolute bottom-20 w-auto min-w-max translate-y-10 scale-0 rounded-lg border border-gray-300 bg-white px-2 py-1 text-xs font-medium text-gray-500 opacity-0 shadow transition-all duration-200 group-hover:translate-y-2 group-hover:scale-100 group-hover:opacity-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:shadow-black/60 group-hover:sm:translate-y-1">
       {text}
     </span>
   );
