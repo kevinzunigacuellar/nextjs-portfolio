7ed3f3f6737b87a2fafe1ab59008ef4982c20f98
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-08 16:27:24 -0500

    decrease skill spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <h2 className="mb-6 text-2xl font-semibold dark:text-white">
           Favorite Technologies
         </h2>
-        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
           {skills.map(({ id, ...rest }) => (
             <Skill
               key={id}
