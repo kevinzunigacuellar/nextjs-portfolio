10e01414bc2a61aa5a9e357245b0f76efd8e9a14
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-08 16:27:48 -0500

    style blog template

--- layouts/BlogLayout.tsx
+++ layouts/BlogLayout.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image';
 import profilePic from 'public/images/me.jpg';
 import { ReactNode } from 'react';
+import Link from 'next/link';
+import { ArrowSmLeftIcon } from '@heroicons/react/solid';
 
 export default function BlogLayout({ children, title, date }:
   { children: ReactNode, title: string, date: string }) {
   return (
     <>
-      <h1 className="text-4xl sm:text-5xl font-sans font-bold mb-6">{title}</h1>
-      <div className="flex space-x-2 mb-10 items-center text-sm sm:text-base">
+      <Link href="/blog">
+        <a className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white inline-flex items-center space-x-1 py-0.5 rounded-full text-sm">
+          <ArrowSmLeftIcon className="h-4" />
+          <p className="">back to blog</p>
+        </a>
+      </Link>
+      <h1 className="text-4xl sm:text-5xl font-sans font-semibold py-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500">{title}</h1>
+      <div className="flex space-x-2 mb-16 items-center justify-center text-gray-600 dark:text-gray-400 text-sm sm:text-base">
         <Image
           className="rounded-full w-full h-full"
           width={24}
@@ -16,22 +24,20 @@ export default function BlogLayout({ children, title, date }:
           alt="Kevin Zuniga Cuellar"
 
         />
-        <p className="dark:text-gray-300">
+        <p className="">
           <span className="mr-2">Kevin</span>
           &middot;
           <time className="ml-2" dateTime={date}>
             {new Date(date).toLocaleDateString('en', {
-              month: 'long',
-              year: 'numeric',
+              dateStyle: 'full',
             })}
           </time>
         </p>
       </div>
-      <main className="prose max-w-none prose-a:text-blue-500 prose-pre:bg-white prose-pre:text-gray-700
+      <main className="prose max-w-none prose-a:text-blue-500 prose-a:no-underline prose-pre:bg-white prose-pre:text-gray-700
       dark:prose-invert dark:prose-a:text-indigo-500 dark:prose-pre:bg-gray-800 dark:prose-pre:text-gray-300"
       >
         {children}
-
       </main>
     </>
   );
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -22,12 +22,12 @@ function Blog({ posts }: { posts: [] }) {
             <a className="group block rounded-xl bg-white p-6 leading-relaxed text-gray-600 shadow hover:shadow-md
             dark:bg-gray-800 dark:text-gray-400 dark:shadow-black/40 transition-shadow duration-300"
             >
-              <h2 className="text-lg font-semibold text-gray-900 group-hover:text-blue-600 dark:text-white
-              dark:group-hover:text-indigo-500"
+              <h2 className="text-lg inline-block font-semibold text-gray-900 group-hover:text-transparent group-hover:bg-clip-text
+              group-hover:bg-gradient-to-r group-hover:from-indigo-500 group-hover:to-pink-500 dark:text-white"
               >
                 {post.title}
               </h2>
-              <time dateTime={post.date} className="text-gray-500 text-sm block">
+              <time dateTime={post.date} className="text-gray-500 dark:text-gray-400 text-sm block">
                 {new Date(post.date).toLocaleDateString('en', {
                   month: 'long',
                   day: '2-digit',
