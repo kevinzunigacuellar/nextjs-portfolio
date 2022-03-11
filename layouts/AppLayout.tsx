18ef99404eeba2755095afaebfa9588229e20fda
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-10 20:32:30 -0500

    reduce mobile padding

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navbar />
-      <main className="mx-auto max-w-3xl px-8 mt-20 mb-36">
+      <main className="mx-auto max-w-3xl px-6 sm:px-8 mt-20 mb-36">
         {children}
         <footer className="mt-10 border-t pt-6 dark:border-gray-800">
           <SpotifyPlayer />
