ab3b29f5c2fe80b593c7cf3fb630bdc7ae51fde8
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-10 19:18:32 -0500

    fix delete method

--- pages/api/guestbook/[id].ts
+++ pages/api/guestbook/[id].ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
     })
 
-    return res.status(204).json({})
+    return res.status(204).end()
   }
 
   if (req.method === 'PUT') {
