113ed46e4e8a5b0c35b7fa155e613487dd7f69af
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-17 18:06:00 -0400

    prettier format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable global-require */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layouts/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   // eslint-disable-next-line import/no-extraneous-dependencies
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
