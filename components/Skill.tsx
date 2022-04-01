87d0c8c01fd913003e281da90d6d6d68b5829dfc
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-01 00:41:49 -0400

    removed skill link href

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,30 +3,18 @@ import { ReactNode } from 'react'
 interface skillProps {
   name: string
   icon: ReactNode
-  href: string
 }
 
-export default function Skill({ icon, name, href }: skillProps) {
+export default function Skill({ icon, name }: skillProps) {
   return (
-    <a
-      className="group flex items-center space-x-3 rounded-xl p-2 transition-colors hover:bg-gray-50
-      dark:hover:bg-gray-800/40"
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a className="flex items-center space-x-3 rounded-xl p-2">
       <span
         className="flex h-10 w-10 items-center justify-center rounded-full bg-white p-2 shadow dark:border-0
         dark:border-t dark:border-gray-700 dark:bg-gray-800"
       >
         {icon}
       </span>
-      <p
-        className="font-normal text-gray-600 group-hover:text-gray-900 dark:text-gray-400
-      dark:group-hover:text-gray-300"
-      >
-        {name}
-      </p>
+      <p className="text-gray-600 dark:text-gray-400">{name}</p>
     </a>
   )
 }
--- content/skills.js
+++ content/skills.js
@@ -9,37 +9,31 @@ const skills = [
   {
     id: 1,
     name: 'TypeScript',
-    href: 'https://www.typescriptlang.org',
     icon: <TypeScript />,
   },
   {
     id: 2,
     name: 'Tailwind CSS',
-    href: 'https://tailwindcss.com',
     icon: <TailwindCSS />,
   },
   {
     id: 3,
     name: 'React',
-    href: 'https://reactjs.org',
     icon: <ReactLogo />,
   },
   {
     id: 4,
     name: 'Next.js',
-    href: 'https://nextjs.org',
     icon: <Nextjs />,
   },
   {
     id: 5,
     name: 'PlanetScale',
-    href: 'https://planetscale.com/',
     icon: <PlanetScale />,
   },
   {
     id: 6,
     name: 'Prisma',
-    href: 'https://www.prisma.io/',
     icon: <Prisma />,
   },
 ]
